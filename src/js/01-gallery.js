58ab5c0cd3882f07f0ba0c592faf51707d361c16
Iurii Gulko <gulko.iurii@gmail.com>
2022-06-06 10:16:11 +0200

    adds js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery');
+const makeGallery = (items) => {
+    const galleryMarkup = [];
+    const item = [...items].map((i) => `
+  <a class="gallery__item" href='${i.original}'>
+    <img
+      class="gallery__image"
+      src='${i.preview}'
+      alt="${i.description}"
+    />
+  </a>
+`).join("");
+    galleryMarkup.push(item);
+    galleryRef.insertAdjacentHTML('afterbegin', galleryMarkup)
+}
+     makeGallery(galleryItems);
 
+
+let lightBox = new SimpleLightbox('.gallery a')
+lightBox.on('show.simplelightbox', function () {
+  lightBox.defaultOptions.captionDelay = 250;
+  lightBox.defaultOptions.captionPosition = 'bottom';
+  lightBox.defaultOptions.captionsData = 'alt';
+})
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onFormData, 500));
+form.addEventListener('submit', onFormsubmit);
+
+const getData = localStorage.getItem(STORAGE_KEY);
+const parseData = JSON.parse(getData);
+
+if (parseData) {
+  form.email.value = parseData.email;
+  form.message.value = parseData.message;
+}
+
+function onFormData() {
+  const email = form.email.value;
+  const message = form.message.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify({ email, message }));
+}
+
+function onFormsubmit(evt) {
+  evt.preventDefault();
+
+  const email = evt.currentTarget.elements.email.value;
+  const message = evt.currentTarget.elements.message.value;
+
+  if (!email || !message) {
+    return alert('Please complete all fields!');
+  }
+
+  const data = {
+    email,
+    message,
+  };
+
+  console.log(data);
+
+  localStorage.removeItem(STORAGE_KEY);
+  evt.currentTarget.reset();
+}
 
